a1624ece87c7473e1892caa31ea6ce281e1772fd
Kelvin R <33947194+goldensrazer@users.noreply.github.com>
2019-01-07 23:06:50 -0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node API!';
 
 module.exports = {
-    connectionString: 'mongodb://kelvin:kelvin123@ds064188.mlab.com:64188/ndstr',
-    sendgridKey: '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',
+    connectionString: 'Sua conexão',
+    sendgridKey: 'SEU KEY SEND',
     containerConnectionString: 'DBMONGO'
 }
